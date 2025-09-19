bed505f4966b247b48a21f0c02e959cd96ad3c1a
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-09-18 23:31:22 -0500

    Added more welcoming texts

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -10,6 +10,9 @@ const SignIn = () => {
         <Image source = {images.onboarding} className = "w-full h-4/6" resizeMode="contain" />
         <View className = "px-10"> 
           <Text className = "text-base text-center uppercase font-rubik text-black-200">Welcome to the ReState</Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">Let's get you closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
