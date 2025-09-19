4e10950b045312125382e5d1e121e878c8bab0c1
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-09-18 23:27:15 -0500

    Added welcoming message under the sign in screen image

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,6 +1,6 @@
 import images from "@/constants/images";
 import React from 'react';
-import { Image, ScrollView } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignIn = () => {
@@ -8,6 +8,9 @@ const SignIn = () => {
     <SafeAreaView className = "bg-white h-full"> 
       <ScrollView contentContainerClassName='h-full'>
         <Image source = {images.onboarding} className = "w-full h-4/6" resizeMode="contain" />
+        <View className = "px-10"> 
+          <Text className = "text-base text-center uppercase font-rubik text-black-200">Welcome to the ReState</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
