c7734a904e4d305a7e3134b04e74841caeb22bb1
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-09-26 15:38:52 -0500

    Updated SafeAreaView;  imported from react-native-safe-area-context

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -13,11 +13,11 @@ const SignIn = () => {
       <ScrollView contentContainerClassName='h-full'>
         <Image source = {images.onboarding} className = "w-full h-4/6" resizeMode="contain" />
         <View className = "px-10"> 
-          <Text className = "text-base text-center uppercase font-rubik text-black-200">Welcome to the ReState</Text>
+          <Text className = "text-base text-center uppercase font-rubik text-black-200">Welcome to the RealState</Text>
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">Let's get you closer to {"\n"}
             <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
-          <Text className="text-lg text-center font-rubik text-black-200 mt-12">Login to ReState with Google</Text>
+          <Text className="text-lg text-center font-rubik text-black-200 mt-12">Login to RealState with Google</Text>
           <TouchableOpacity onPress={handleLogin} className="bg-blue-100 shadow-lg shadow-zinc-300 rounded-full w-full py-4 mt-1">
             <View className="flex flex-row items-center justify-center space-x-3">
               <Image
